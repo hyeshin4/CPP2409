e529011aad4d5c820e65e3a89ef5a2d4581aaf4c
hyeshin4 <kimhyeshin1234@gmail.com>
2024-09-30 16:41:27 +0900

    Array Exercises2

--- challenge/week5/select_customer2.cpp
+++ challenge/week5/select_customer2.cpp
@@ -31,30 +31,34 @@ int main()
             min = ages[i];
             minName = names[i];
     }
+    }
+    for (int i = 0; i < maxPeople; i++){
         if (ages[i] == max)
-            oldPeople[i] += ages[i];
+            oldPeople[i] += names[i];
         if (ages[i] == min)
-            youngPeople[i] += ages[i];
+            youngPeople[i] += names[i];
 
     }
 
     int choice;
     do {
-        cout <<"메뉴를 선택하세요 (1. 나이가 가장 많은 사람 , 2. 나이가 가장 적은 사람 , 3. 종료): ";
+        cout <<endl<<"메뉴를 선택하세요 (1. 나이가 가장 많은 사람 , 2. 나이가 가장 적은 사람 , 3. 종료): ";
         cin >> choice;
 
         switch(choice){
             case 1:
-                cout << maxName<< " (" << max << "세) "<< endl;
+                for (int i = 0; i < maxPeople; i++){
+                cout <<oldPeople[i]<<" ";}
                 break;
             case 2:
-                cout << minName<< " (" << min << "세) "<< endl;
+                for (int i = 0; i < maxPeople; i++){
+                cout <<youngPeople[i]<<" ";}
                 break;
             case 3:
-                cout <<"프로그램 종료" << endl;
+                cout <<"프로그램 종료" ;
                 break;
             default:
-                cout <<"잘못된 선택" << endl;
+                cout <<"잘못된 선택";
                 break;
         }
     } while (choice != 3 );
