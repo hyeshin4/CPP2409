f8f454f9bc404eff1d9d8b226b597ba527245109
hyeshin4 <kimhyeshin1234@gmail.com>
2024-11-13 16:33:01 +0900

    Copy Constructor and Static

---
+++ practice/week11/has_a.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Date
+{
+    int year, month, day;
+
+public:
+    Date(int y, int m, int d) : year(y), month(m), day(d) {}
+
+    void Print() const
+    {
+        cout << year << "." << month << "." << day << endl;
+    }
+};
+
+class Person
+{
+    string name;
+    Date birth;
+
+public:
+    Person(string n, Date d) : name(n), birth(d) {}
+
+    void Print() const
+    {
+        cout << name << ": ";
+        birth.Print();
+    }
+};
+
+int main()
+{
+    Date d{1998, 3, 1};
+    Person p{"김철수", d};
+    p.Print();
+    return 0;
+}
